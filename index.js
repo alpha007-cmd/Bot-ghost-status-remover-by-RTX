dea6edd3d60998dd7f1d35085b901140e5daa343
alpha <143378256+alpha007-cmd@users.noreply.github.com>
2024-01-25 14:22:05 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching Marvel Custom","Playing Z-Modular","Playing Blender","Playing Photoshop",];
 
 
 let currentIndex = 0;
